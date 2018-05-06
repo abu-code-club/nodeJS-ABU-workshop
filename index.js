f9f9e32856bafda02b8e9a72428cf7bfcb2cf21d
mustii <mustii@mmk.one>
2018-05-06 22:23:29 +0300

    Passport created

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const server = express();
+var bodyParser = require("body-parser");
+const morgan = require('morgan');
+var path = require('path');
+// const router = require('./routes/user.js');
+const PORT = process.env.PORT || 8080;
+
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
+
+server.use(express.static('webpage/Helpers'));
+server.set('views', path.join(__dirname, '/webpage/views'));
+server.set('view engine', 'pug')
+
+server.use(morgan("short"));
+// server.use(router)
+
+
+server.get('/', (req, res) => {
+    res.render('home')
+})
+
+server.get('/login', (req,res) => {
+    res.render('login')
+})
+
+server.get('/signup', (req,res) => {
+    res.render('signup')
+})
+
+server.listen(PORT,() => {
+    console.log("server running");
+});
+
