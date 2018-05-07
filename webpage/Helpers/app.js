861cddebf6bcfaf1cf7adc740c906dc008519c1d
mustiikhalil <mustii@mmk.one>
2018-05-07 17:19:01 +0300

    Working server (#6)
    * template done! now working on the final
    
    * Made foldering better
    
    * Authentication done
    
    * Running with cookies
    
    * Running with cookies
    
    * Added Database Module
    
    * Added the UI for todo
    
    * Fixed passport
    
    * Master done

--- webpage/Helpers/app.js
+++ webpage/Helpers/app.js
@@ -1,13 +1,26 @@
 
 function redirect(page){
-    location.href = location.href + page
-};
+    location.href = location.href + page;
+}
 
 function validation() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
-    if (password.length > 6 && password.indexOf(/^(.*[A-Z].*)$/) && username.length > 5){
+    if (password.length >= 6 && password.indexOf(/^(.*[A-Z].*)$/) && username.length >= 5){
         return true;
     }
     return false;
+}
+
+function addNewTodo(){
+    var todo = {"todo":document.getElementById('todo').value};
+    // $.ajax({
+    //     type: "POST",
+    //     url: "http://localhost:8080/add-todo",
+    //     data: todo,
+    //     success: function(){
+    //         location.href = location.href
+    //     }
+    // })
+    
 }
